40240959ad4747709d47491cc08f8537f751ecd5
bharath703 <bharath.hemaprakash@gmail.com>
2018-07-26 02:21:18 -0500

    This is the updated version of the project

---
+++ NLPArticleAnalyzer/src/main/java/com/mycompany/nlparticleanalyzer/PortfolioNewsAnalyzer.java
@@ -0,0 +1,113 @@
+package com.mycompany.nlparticleanalyzer;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Bharath
+ */
+
+import java.util.*;
+import java.net.URL;
+import java.io.IOException;
+
+import org.xml.sax.SAXException;
+import edu.stanford.nlp.tagger.maxent.MaxentTagger;
+import de.l3s.boilerpipe.document.TextDocument;
+import de.l3s.boilerpipe.extractors.CommonExtractors;
+import de.l3s.boilerpipe.sax.BoilerpipeSAXInput;
+import de.l3s.boilerpipe.sax.HTMLDocument;
+import de.l3s.boilerpipe.sax.HTMLFetcher;
+import de.l3s.boilerpipe.BoilerpipeProcessingException;
+import edu.stanford.nlp.util.StringUtils;
+
+
+public class PortfolioNewsAnalyzer {
+    private HashSet<String> portfolio;
+    private static final String modelPath = "edu\\stanford\\nlp\\models\\pos-tagger\\english-left3words\\english-left3words-distsim.tagger";
+    private MaxentTagger tagger;
+
+    public PortfolioNewsAnalyzer() {
+        portfolio = new HashSet<String>();
+        tagger = new MaxentTagger(modelPath);
+    }
+    
+    public void addPortfolioCompany(String company) {
+        portfolio.add(company);
+    }
+
+    public boolean analyzeArticle(String urlString) throws
+            IOException,
+            SAXException,
+            BoilerpipeProcessingException
+    {
+        String articleText = PortfolioNewsAnalyzer.extractFromUrl(urlString);
+//       System.out.println(articleText);
+        String tagged = tagPos(articleText);
+//        System.out.println("");
+//        System.out.println("");
+//        System.out.println("");
+//        System.out.println(tagged);
+//        System.out.println("");
+//        System.out.println("");
+//        System.out.println("");
+        
+        
+        HashSet<String> properNounsSet = PortfolioNewsAnalyzer.extractProperNouns(tagged);
+//        System.out.println("");
+//        System.out.println("");
+//        System.out.println("");
+//        System.out.println(properNounsSet);
+//        System.out.println("");
+//        System.out.println("");
+//        System.out.println("");
+        return arePortfolioCompaniesMentioned(properNounsSet);
+    }
+
+    public boolean arePortfolioCompaniesMentioned(HashSet<String> articleProperNouns){
+        return !Collections.disjoint(articleProperNouns, portfolio);
+    }
+    
+    public static HashSet<String> extractProperNouns(String taggedOutput) {
+        HashSet<String> propNounSet = new HashSet<String>();
+        String[] split = taggedOutput.split(" ");
+        List<String> propNounList = new ArrayList<String>();
+        for (String token: split){
+            String[] splitTokens = token.split("_");
+            if(splitTokens.length < 2) {
+                continue;
+            }
+            if(splitTokens[1].equals("NNP")){
+                propNounList.add(splitTokens[0]);
+            } else {
+                addProperNounToSet(propNounSet, propNounList);
+            }
+        }
+        addProperNounToSet(propNounSet, propNounList);
+        return propNounSet;
+    }
+
+    private static void addProperNounToSet(HashSet<String> propNounSet, List<String> propNounList) {
+        if (!propNounList.isEmpty()) {
+            propNounSet.add(StringUtils.join(propNounList, " "));
+            propNounList.clear();
+        }
+    }
+
+    public String tagPos(String input) {
+        return tagger.tagString(input);
+    }
+
+    public static String extractFromUrl(String userUrl) throws
+            IOException,
+            SAXException,
+            BoilerpipeProcessingException  {
+        final HTMLDocument htmlDoc = HTMLFetcher.fetch(new URL(userUrl));
+        final TextDocument doc = new BoilerpipeSAXInput(htmlDoc.toInputSource()).getTextDocument();
+        return CommonExtractors.ARTICLE_EXTRACTOR.getText(doc);
+    }
+}
